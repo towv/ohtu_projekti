570f6664b723c2f2146f6276dc4c2198cd4ef3b4
Topi Laanti <laantop@gmail.com>
2017-11-16 13:14:23 +0200

    Travis conffaus

--- UnitTestFiles/Test/KirjaTest.php
+++ UnitTestFiles/Test/KirjaTest.php
@@ -1,12 +1,22 @@
 <?php
-namespace UnitTestFiles\Test;
-use PHPUnit\Framework\TestCase;
- 
-class KirjaFTest extends TestCase {
+class KirjaTest extends PHPUnit_Framework_TestCase {
+	@Test
+   	public function testAll() {
+      // Allocate a test fixture.
+      $fixture = array();
+      // Assert initially empty, count() is 0.
+      $this->assertEquals(0, count($fixture));
 
-    public function testTrueCheck() {
-    $condition = true;
-    $this->assertTrue($condition);
+      // Test array_push().
+      // Push an item into array.
+      array_push($fixture, 'foo');
+      // Assert one item, count() is 1.
+      $this->assertEquals(1, count($fixture));
+      // Assert item pushed.
+      $this->assertEquals('foo', $fixture[count($fixture)-1]);
+ 
+      // Test array_pop().
+      $this->assertEquals('foo', array_pop($fixture));
+      $this->assertEquals(0, count($fixture));
+   }
 }
-}
-?>
