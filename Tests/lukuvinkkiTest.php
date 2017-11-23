805258d1def967415f8fcf0beaf855688fee0b5e
Topi Laanti <laantop@gmail.com>
2017-11-23 12:00:39 +0200

    Testien kanssa häsläystä

--- Tests/lukuvinkkiTest.php
+++ Tests/lukuvinkkiTest.php
@@ -1,11 +1,15 @@
 <?php
 include("app/models/lukuvinkki.php");
+include("lib/database.php");
 
 class lukuvinkkiTest extends PHPUnit_Framework_TestCase
 {
-  public function testLuontiOnnistuuOikein()
-  {
-    $lukuvinkki = new Lukuvinkki(array(
+
+  public $lukuvinkki;
+
+  public function setUp()
+    {
+       $this->lukuvinkki = new Lukuvinkki(array(
       'id' => '111',
       'otsikko' => 'samin kuntopolku',
       'tekija' => 'sami',
@@ -15,9 +19,24 @@ class lukuvinkkiTest extends PHPUnit_Framework_TestCase
       'kuvaus' => 'kuntoillaan yhdessä t sami',
       'julkaistu' => '1994'
     ));
-    $result = $lukuvinkki->otsikko;
+    }
 
-    $this->assertEquals("samin kuntopolku", $result);
+
+  public function testLuontiOnnistuuOikein()
+  {
+  
+
+    
+
+    $this->assertEquals("samin kuntopolku", $this->lukuvinkki->otsikko);
+  }
+  
+  public function testSaveToimii() 
+  {
+    $query = DB::connection() -> prepare('INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu) VALUES (:otsikko, :tekija, :isbn, :url, :tyyppi, :kuvaus, :julkaistu) RETURNING id');
+    $query -> execute(array('otsikko' => $this->lukuvinkki->otsikko, 'tekija' => $this->lukuvinkki->tekija, 'isbn' => $this->lukuvinkki->isbn, 'url' => $this->lukuvinkki->url, 'tyyppi' => $this->lukuvinkki->tyyppi, 'kuvaus' => $this->lukuvinkki->kuvaus, 'julkaistu' => $this->lukuvinkki->julkaistu));
+    $row = $query->fetch();
+    $this->id = $row['id'];
   }
 
 }
