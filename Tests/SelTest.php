7ae5abb36abeeaaf1d5a3a2eda84d9109f158c0d
Joakim Jaulimo <joakim.jaulimo@gmail.com>
2017-11-30 11:02:10 +0200

    selenium

---
+++ Tests/SelTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'vendor/autoload.php';
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://laatopi.users.cs.helsinki.fi/tsoha/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('http://laatopi.users.cs.helsinki.fi/tsoha/');
+        $this->assertEquals('Gepardi-OhTu', $this->title());
+    }
+
+}
+?>
