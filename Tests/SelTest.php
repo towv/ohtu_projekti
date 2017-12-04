c1045a003b3cf3f54acaf22f172c6d1788f627d9
Lotta Merisaari <merisaari.lotta@gmail.com>
2017-12-04 23:51:23 +0200

    lukuvinkkien viimeisimmät muutokset

--- Tests/SelTest.php
+++ Tests/SelTest.php
@@ -1,47 +1,16 @@
 <?php
 require_once 'vendor/autoload.php';
-class WebTest extends PHPUnit_Framework_TestCase
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-
-  /**
-   * @var \RemoteWebDriver
-   */
     protected function setUp()
     {
-        $capabilities = array(\WebDriverCapabilityType::BROWSER_NAME => 'chrome');
-        $this->webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $capabilities);
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://laatopi.users.cs.helsinki.fi/tsoha/');
     }
-
-    protected $url = 'http://laatopi.users.cs.helsinki.fi/tsoha/';
-
     public function testTitle()
     {
-        $this->webDriver->get($this->url);
-        $this->assertContains('Gepardi-OhTu', $this->webDriver->getTitle());
+        $this->url('http://laatopi.users.cs.helsinki.fi/tsoha/');
+        $this->assertEquals('Gepardi-OhTu', $this->title());
     }
-
-    public function tearDown()
-    {
-        $this->webDriver->quit();
-    }
-
-    public function testSignUp()
-    {
-      $this->webDriver->get($this->url);
-      sleep(5);
-      $search = $this->webDriver->findElement(WebDriverBy::id('signup'));
-      $search->click();
-      sleep(5);
-      $search = $this->webDriver->findElement(WebDriverBy::id('tunnus'));
-      $search->click();
-      $this->webDriver->getKeyboard()->sendKeys('php-webdriver');
-      $search = $this->webDriver->findElement(WebDriverBy::id('salasana'));
-      $search->click();
-      $this->webDriver->getKeyboard()->sendKeys('php-webdriver');
-      $search = $this->webDriver->findElement(WebDriverBy::id('submit'));
-      $search->click();
-      sleep(5);
-    }
-
 }
 ?>
--- app/models/lukuvinkki.php
+++ app/models/lukuvinkki.php
@@ -1,6 +1,6 @@
 <?php
 
-//include("lib/base_model.php");
+include("lib/base_model.php");
 
 class Lukuvinkki extends BaseModel {
 
